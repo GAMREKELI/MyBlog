294d26002dc68ef261852508c6cf3874764f1fed
GAMREKELI <nekkimark2@gmail.com>
2023-08-09 23:30:32 +0300

    add eureka server for project

---
+++ article/src/main/java/ru/gamrekeli/article/ArticleApplication.java
@@ -0,0 +1,13 @@
+package ru.gamrekeli.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleApplication.class, args);
+	}
+
+}
